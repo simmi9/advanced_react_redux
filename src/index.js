1bee4b77182fc5e0d4212a092ac6f7ad96aa1a68
simmi9 <bhawna.keswani20@gmail.com>
2019-07-17 13:50:50 -0400

    using increment and decrement for seperate button component

--- src/index.js
+++ src/index.js
@@ -25,21 +25,39 @@ ReactDOM.render(element2, document.querySelector("#root"));  */
 
 //Creating Component Class
 class Counter extends React.Component {
-    state = { count: this.props.count};
-    click = () => {
+    state = { count: this.props.count };  
+    click = (incr) => {
        // this.setState((prevState)=>({count: prevState.count + 1}));
-       this.setState({count: this.state.count + 1});  
+       this.setState({count: this.state.count + incr});  
       //  this.forceUpdate(); //used only when react not handling state changes
     }
-  
+    
+    change = (e) => {
+        if(Number(e.target.value) || e.target.value === "0") {
+        this.setState({count: e.target.value});
+        } 
+    }
     render(){
         return (
             <>
-            <h1> The value is: {this.props.count} </h1>
-            <button onClick={this.click}>Increment</button>
+            <h1> The value is: {this.state.count} </h1>
+            <input type="text" 
+                   value={this.state.count}
+                   onChange={this.change}
+                   readOnly>
+            </input>
+            <MyButton increment={1} onClick={this.click}/> 
+            <MyButton increment={-10} onClick={this.click}/>
+            <MyButton increment={100} onClick={this.click}/>
             </>
         )
-    }
+    }  
+}
+
+function MyButton(props){
+    return <button style={(props.increment<0) ? {backgroundColor: "red"} : {} } onClick={()=>{props.onClick(props.increment)}}>
+    {props.increment < 0 ? "decrement" : "increment"} 
+    {Math.abs(props.increment)}</button>  
 }
 ReactDOM.render( <Counter count={5}/>, 
                 document.querySelector("#root"));
